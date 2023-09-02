08f12419dceae212c9149cc54e173f626f1925a3
Buranovv <zahiriddinburanov007@gmail.com>
2023-09-02 12:54:22 +0500

    fix

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-function findWord(word) {
-  const arr = word.split("");
-  console.log(arr);
-  let obj = {};
-  arr.forEach((letter) => {
-    if (!Object.keys(obj).includes(letter)) {
-      obj[letter] = 1;
-    } else {
-      obj[letter] = obj[letter] + 1;
-    }
-  });
-  console.log(obj);
-}
 
-findWord("nmagap");
